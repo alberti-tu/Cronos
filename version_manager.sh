6facecf44e90072db399524b4ec2dd1f156fac9c
Albert Álvarez <albertalvarezmiron@gmail.com>
2023-05-23 10:18:36 +0200

    new version manager

--- version_manager.sh
+++ version_manager.sh
@@ -1,17 +1,31 @@
 #!/bin/bash
+if [ $# -ne 2 ]; then
+  echo "Uso: $0 <minimum_version> <current_version>"
+  exit 1
+fi
 
-min_version=$1
+minimum_version=$1
 current_version=$2
 
-if [ "$min_version" -gt "$current_version" ]; then
-    echo "$min_version"
-else
-    IFS='.' read -r -a version_parts <<< "$current_version"
-    major="${version_parts[0]}"
-    minor="${version_parts[1]}"
-    patch="${version_parts[2]}"
-    
-    ((patch++))
-    next_version="$major.$minor.$patch"
-    echo "$next_version"
-fi
+IFS='.' read -ra arr_minimum <<< "$minimum_version"
+IFS='.' read -ra arr_current <<< "$current_version"
+
+for i in "${!arr_minimum[@]}"; do
+  if [ "${arr_minimum[$i]}" -gt "${arr_current[$i]}" ]; then
+    echo "$minimum_version"
+    exit 0
+  elif [ "${arr_minimum[$i]}" -lt "${arr_current[$i]}" ]; then
+    break
+  fi
+done
+
+last_index=$(( ${#arr_current[@]} - 1 ))
+arr_current[$last_index]=$(( ${arr_current[$last_index]} + 1 ))
+
+new_version="${arr_current[0]}"
+for (( i=1; i<=$last_index; i++ )); do
+  new_version="${new_version}.${arr_current[$i]}"
+done
+
+echo "$new_version"
+exit 0
