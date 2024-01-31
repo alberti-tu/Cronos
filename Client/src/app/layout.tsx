699ab7e994e446eab3dad9ca0845530017d6e80a
Albert Álvarez <albertalvarezmiron@gmail.com>
2024-01-31 12:17:34 +0100

    client init

---
+++ Client/src/app/layout.tsx
@@ -0,0 +1,19 @@
+import type { Metadata } from "next"
+
+export const metadata: Metadata = {
+  title: "Cronos"
+}
+
+type RootProps = {
+  children: React.ReactNode
+}
+
+function RootLayout({ children }: Readonly<RootProps>) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
+
+export default RootLayout
---
+++ Client/src/app/page.tsx
@@ -0,0 +1,9 @@
+function Home() {
+  return (
+    <main>
+      <h1>My app</h1>
+    </main>
+  )
+}
+
+export default Home
